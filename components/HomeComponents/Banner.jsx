b22c9cbfe984c75e1cdd8397e16221bfe39d3425
sumitbopewar <91839519+sumitbopewar@users.noreply.github.com>
2024-10-24 16:48:43 +0530

    fifth commit

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -41,22 +41,22 @@ const Banner = () => {
                 <div className="grid grid-cols-2 gap-4 md:gap-0 md:flex items-center justify-between w-full px-4 xl:px-8 2xl:px-16">
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>10+</span>
+                        <span className='text-base md:text-lg text-Green font-bold'>3+</span>
                         <span className='text-xs text-Snow'>Completed Projects</span>
                     </div>
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>8+</span>
+                        <span className='text-base md:text-lg text-Green font-bold'>1+</span>
                         <span className='text-xs text-Snow'>Freelance Clients</span>
                     </div>
 
-                    <div className="flex items-center gap-x-1">
+                    {/* <div className="flex items-center gap-x-1">
                         <span className='text-base md:text-lg text-Green font-bold'>5+</span>
                         <span className='text-xs text-Snow'>Honors & Awards</span>
-                    </div>
+                    </div> */}
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>10+</span>
+                        <span className='text-base md:text-lg text-Green font-bold'>5+</span>
                         <span className='text-xs text-Snow'>Opensource Projects</span>
                     </div>
 
