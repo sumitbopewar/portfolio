2c3344d591fed080e6dacc9f0de57f4d8e0112e0
sumitbopewar <91839519+sumitbopewar@users.noreply.github.com>
2025-01-27 14:35:46 +0530

    [ Type in the commit message]

--- pages/api/background.js
+++ pages/api/background.js
@@ -24,8 +24,8 @@ const background = [
                 title: 'ICEICO Technologies Pvt Ltd.',
                 role: 'Frontend Developer',
                 url: 'https://iceico.in/',
-                desc: 'I began my career as a Trainee, where I spent the first six months honing my foundational skills in web development, focusing on React.js, JavaScript, and frontend technologies. After successfully completing the training period, I transitioned into a full-time React.js Developer role. I have total 3 years of experience and I have gained extensive hands-on experience in building dynamic, responsive web applications using React.js, managing state with Redux, optimizing performance, and integrating APIs. I have also contributed to large-scale projects, collaborated closely with cross-functional teams, and continuously improved user experiences through innovative solutions.',
-                year: 'June/2021 - June/2024',
+                desc: 'I began my career as a Trainee, where I spent the first six months honing my foundational skills in web development, focusing on React.js, JavaScript, and frontend technologies. After successfully completing the training period, I transitioned into a full-time React.js Developer role. I have total 1.6 years of experience and I have gained extensive hands-on experience in building dynamic, responsive web applications using React.js, managing state with Redux, optimizing performance, and integrating APIs. I have also contributed to large-scale projects, collaborated closely with cross-functional teams, and continuously improved user experiences through innovative solutions.',
+                year: 'April/2023 - September/2024',
                 location: 'Nagpur, INDIA'
             }
         ]
